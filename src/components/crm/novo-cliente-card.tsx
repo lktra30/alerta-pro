4995fd60efe4b3dc5032d9077aa262c2fc7a1c42
Lucass14js <businesslucassoliveira@gmail.com>
2025-08-08 16:22:47 -0300

    fix field number

--- src/components/crm/novo-cliente-card.tsx
+++ src/components/crm/novo-cliente-card.tsx
@@ -190,14 +190,30 @@ export function NovoClienteCard({ onClienteAdicionado, isOpen, onOpenChange }: N
     // Remove all non-digits
     const digits = value.replace(/\D/g, '')
     
-    // Apply mask: (XX) XXXXX-XXXX
-    if (digits.length <= 11) {
-      return digits.replace(/(\d{2})(\d{5})(\d{4})/, '($1) $2-$3')
-        .replace(/(\d{2})(\d{4})(\d{0,4})/, '($1) $2-$3')
-        .replace(/(\d{2})(\d{0,5})/, '($1) $2')
-        .replace(/(\d*)/, '$1')
+    // Don't format if too long
+    if (digits.length > 11) {
+      return value.substring(0, 15) // Keep maxLength working
     }
-    return value
+    
+    // Format based on length
+    if (digits.length >= 11) {
+      // 11 digits: (XX) 9XXXX-XXXX (mobile)
+      return digits.replace(/(\d{2})(\d{5})(\d{4})/, '($1) $2-$3')
+    } else if (digits.length >= 10) {
+      // 10 digits: (XX) XXXX-XXXX (landline)
+      return digits.replace(/(\d{2})(\d{4})(\d{4})/, '($1) $2-$3')
+    } else if (digits.length >= 7) {
+      // 7-9 digits: partial formatting
+      return digits.replace(/(\d{2})(\d{4})(\d{0,4})/, '($1) $2-$3')
+    } else if (digits.length >= 3) {
+      // 3-6 digits: area code + start
+      return digits.replace(/(\d{2})(\d{0,5})/, '($1) $2')
+    } else if (digits.length >= 1) {
+      // 1-2 digits: just area code
+      return digits.replace(/(\d{0,2})/, '($1')
+    }
+    
+    return digits
   }
 
   const resetForm = () => {
